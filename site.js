45de7b990936b8353d44093707e649c268dedc75
Zoltan Molnar <molnarz1989@gmail.com>
2016-04-12 16:08:20 +0200

    Build finished

--- site.js
+++ site.js
@@ -4,15 +4,4 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    $( "#show-login" ).click(function() {
-        if ( $( ".login-box" ).hasClass("opened") ) {
-            $( ".login-box" ).removeClass("opened").slideUp( "slow", function() {
-                // Animation complete.
-            });
-        } else {
-            $( ".login-box" ).addClass("opened").slideDown( "slow", function() {
-                // Animation complete.
-            });
-        }
-    });
 });
